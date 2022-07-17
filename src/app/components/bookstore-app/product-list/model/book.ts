7f7d27a7556f6e39dbe10b43f037d55acb0f68bc
Francisco Weslley <84885942+franciscowesll@users.noreply.github.com>
2022-07-17 00:26:13 -0300

    Add files via upload

---
+++ src/app/components/bookstore-app/product-list/model/book.ts
@@ -0,0 +1,11 @@
+import { SubscriptionLoggable } from "rxjs/internal/testing/SubscriptionLoggable";
+
+export class Book
+{
+    id!: string;
+    category!: string;
+    name!: string;
+    price!: string;
+    quantity!: string;
+    img!: string;
+}
